cb813111a11442650691b2e3ff22476f124190ec
ShumWengSang <ballwallcallall@gmail.com>
2015-01-16 18:26:01 +0800

    End of First Day

---
+++ Knightmare/Assets/KnightManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KnightManager : MonoBehaviour {
+	//1-8 for x
+	//0-7 for y
+
+	bool Selected;
+	Vector2 Pos;
+	// Use this for initialization
+	void Start () {
+		Selected = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Selected)
+		{
+
+		}
+	}
+}
---
+++ Knightmare/Assets/Script/BoardScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardScript : MonoBehaviour {
+
+	public class BoardPartInfo 
+	{
+		public Vector3 Pos;
+		public bool ChessOnPiece;
+		BoardPartInfo()
+		{
+			ChessOnPiece = false;
+		}
+	}
+	BoardPartInfo[][] theBoardPart;
+
+	// Use this for initialization
+	void Start () {
+		theBoardPart = new BoardPartInfo[8][8];
+
+		for (float a = 0.5, i = 0; i < 8; i++, a++) {
+			for(float b = 0.5, j = 0; j < 8; j++, b++) {
+				theBoardPart[i][j].Pos.x = a;
+				theBoardPart[i][j].Pos.y = b;
+				}
+			}
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
